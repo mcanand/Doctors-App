3136005fa278ac6a417b3ec90342f391ba1bcf05
mcanand <mcanand1998@gmail.com>
2023-09-06 21:15:12 +0530

    [UPD][Vedo Call Issue]

--- doctors_app/models/doctor_time_slot.py
+++ doctors_app/models/doctor_time_slot.py
@@ -13,7 +13,7 @@ class Doctor(models.Model):
     _description = 'Doctor Time Slots'
 
     doctor_id = fields.Many2one('hr.employee', string='Doctor')
-    partner_ids = fields.Many2many('res.partner', string='Partner', default=False)
+    partner_ids = fields.Many2many('res.partner', string='Partner')
     display_time_interval = fields.Char(string='Time Interval')
     booking_button = fields.Boolean(string='Assigned', default=False)
     time_from = fields.Float(string='From', related='doctor_id.time_from', readonly=True)
@@ -37,9 +37,10 @@ class Doctor(models.Model):
     @api.depends('partner_ids')
     def _compute_multiple_partners(self):
         for slot in self:
-            slot.multiple_partners = len(slot.partner_ids) > 1
-
-
+            if len(slot.partner_ids) > 1:
+                slot.multiple_partners = True
+            else:
+                slot.multiple_partners = False
 
     def view_prescription(self):
         prescription = self.patient_prescription_ids[0] if self.patient_prescription_ids else False
@@ -99,27 +100,44 @@ class Doctor(models.Model):
 
     # Set boolean button true and assign meeting link while booking
     @api.onchange('partner_ids')
-    def on_partner_id_change(self):
+    def on_partner_ids_change(self):
         if self.partner_ids:
             self.booking_button = True
-            partners_to = [self.doctor_id.user_id.partner_id.id] + self.partner_ids.ids
-            name = ''
-            if self.multiple_partners:
-                name += "Group Meeting " + self.doctor_id.name
-            else:
-                name += "Meeting " + self.doctor_id.name
-            url = self.create_meeting_link(partners_to, name)
-            self.meeting_link = url
         else:
             self.booking_button = False
 
-    def create_meeting_link(self, partners_to, name):
+
+    def write(self, vals):
+        if vals.get('booking_button') and vals.get('partner_ids'):
+            for rec in self:
+                default_display_mode = 'video_full_screen'
+                partners_to = [rec.doctor_id.user_id.partner_id.id] + vals.get('partner_ids')[0][2]
+                name = ''
+                if vals.get('multiple_partners'):
+                    name += "Group Meeting " + rec.doctor_id.name
+                else:
+                    name += "Meeting " + rec.doctor_id.name
+                meet = rec.env['mail.channel'].create_group(partners_to, default_display_mode, name)
+                base_url = rec.env['ir.config_parameter'].get_param('web.base.url')
+                url = base_url + '/chat/' + str(meet.get('id')) + '/' + meet.get('uuid')
+                vals.update({'channel_id': meet.get('id')})
+                vals.update({'meeting_link': url})
+        res = super(Doctor, self).write(vals)
+        return res
+
+    def create_meeting_link(self):
         default_display_mode = 'video_full_screen'
+        partners_to = [self.doctor_id.user_id.partner_id.id] + self.partner_ids.ids
+        name = ''
+        if self.multiple_partners:
+            name += "Group Meeting " + self.doctor_id.name
+        else:
+            name += "Meeting " + self.doctor_id.name
         vals = self.env['mail.channel'].create_group(partners_to, default_display_mode, name)
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
-        self.channel_id = vals.get('id')
         url = base_url + '/chat/' + str(vals.get('id')) + '/' + vals.get('uuid')
-        return url
+        self.channel_id = vals.get('id')
+        self.meeting_link = url
 
     # booking validation
     @api.constrains('doctor_id', 'date', 'from_time', 'to_time', 'partner_ids')
